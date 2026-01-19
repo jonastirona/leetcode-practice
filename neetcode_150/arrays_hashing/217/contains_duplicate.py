2ef32db7ad57fa0e71f243e1495c16fad17929f3
jonastirona <jonastirona1@gmail.com>
2026-01-19 09:22:58 -0500

    add missing explanations for completed problems 217 and 242

--- neetcode_150/arrays_hashing/217/contains_duplicate.py
+++ neetcode_150/arrays_hashing/217/contains_duplicate.py
@@ -1,4 +1,6 @@
 # Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+# Logic: use hashmap to keep track of seen nums, returning false if character has already been seen while iterating through num.
 from collections import defaultdict
 
 class Solution:
--- neetcode_150/arrays_hashing/242/valid_anagram.py
+++ neetcode_150/arrays_hashing/242/valid_anagram.py
@@ -1,4 +1,6 @@
 # Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+# Logic: First, if they are differing lengths return false. If they are both empty, return True. Second, use hashmap to store (character, count) for word 1. then, iterate through word 2 and for each character, if exists in hashmap, decrement the count. if the count < 0 (the letter occurs more in word 2) or the character doesn't exist in hashmap, return false. also return false if at the end of iterating through word 2, the count of a character in hasmap > 0. If not all that, return True. 
 from collections import defaultdict
 
 class Solution:
